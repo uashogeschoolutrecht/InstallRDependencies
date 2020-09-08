52bb9a9f7718904726b84edfe24700c2f15eda36
Marc <marc.teunis@hu.nl>
2020-09-08 19:06:30 +0200

    Update essentials2.bash

--- essentials2.bash
+++ essentials2.bash
@@ -11,12 +11,20 @@ apt-get update \
     lsb-release \
     libxml2-dev \
     libcurl4-openssl-dev \
+    git \
+    ffmpeg \
+    openjdk-11-jdk 
+    openjdk-11-jre 
+    libcurl4-gnutls-dev
     multiarch-support \
     psmisc \
     procps \
     python-setuptools \
     sudo \
     wget \
+
+apt -y autoremove
+#apt-get upgrade
 
 ## add missing R-packages in the DESCRIPTION file under imports. Github or other remotes go in the Remotes field:
 Rscript -e "options(repos = c(CRAN = 'https://cran.r-project.org')); install.packages(c('devtools'))"
