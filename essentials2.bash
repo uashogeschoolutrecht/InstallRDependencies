fc373969c16f8679401287b00754f4d1f4d95698
Marc <marc.teunis@hu.nl>
2020-09-08 16:31:42 +0200

    Update essentials2.bash

--- essentials2.bash
+++ essentials2.bash
@@ -1,2 +1,21 @@
+## add missing UBUNTU depends here
+apt-get update \
+  && apt-get install -y --no-install-recommends \
+    file \
+    git \
+    libapparmor1 \
+    libclang-dev \
+    libcurl4-openssl-dev \
+    libedit2 \
+    libssl-dev \
+    lsb-release \
+    multiarch-support \
+    psmisc \
+    procps \
+    python-setuptools \
+    sudo \
+    wget \
+
+## add missing R-packages in the DESCRIPTION file under imports. Github or other remotes go in the Remotes field:
 Rscript -e "options(repos = c(CRAN = 'https://cran.r-project.org')); install.packages(c('devtools', 'remotes', 'BiocManager'))"
 Rscript -e "options(repos = c(CRAN = 'https://cran.r-project.org')); devtools::install('.', dependencies=TRUE, build_vignettes=FALSE, repos = BiocManager::repositories())"
